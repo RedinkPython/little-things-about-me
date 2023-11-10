95888f088e7955896b1adbd2bfdc891e481bde47
Cesar Gabriel <121568700+RedinkPython@users.noreply.github.com>
2023-11-10 17:30:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ basic.show_string("My name is:")
 basic.show_string("Cesar!")
 basic.show_string("My age is:")
 basic.show_number(11)
+basic.show_leds("""
+    # # # # #
+    . # . # .
+    . # . # .
+    . . . . .
+    . # # # .
+    """)
