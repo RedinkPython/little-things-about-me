dfb1ce31425b5246398ce20e3b1950fa1ce81f7b
Cesar Gabriel <121568700+RedinkPython@users.noreply.github.com>
2023-11-10 17:29:44 -0600

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+basic.show_string("My name is:")
+basic.show_string("Cesar!")
+basic.show_string("My age is:")
+basic.show_number(11)
---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showString("My name is:")
+basic.showString("Cesar!")
+basic.showString("My age is:")
+basic.showNumber(11)
+basic.showLeds(`
+    # # # # #
+    . # . # .
+    . # . # .
+    . . . . .
+    . # # # .
+    `)
